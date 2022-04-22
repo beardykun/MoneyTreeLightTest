917452db873f76fa189eec901ce38e135dfd1872
Mikhail <beardykun@yahoo.com>
2022-04-22 23:48:01 +0900

    added AccountFragment its viewModel and layout files

---
+++ app/src/main/java/com/example/moneytreelighttest/accounts/AccountsFragment.kt
@@ -0,0 +1,8 @@
+package com.example.moneytreelight.accounts
+
+import androidx.fragment.app.Fragment
+import com.example.moneytreelight.R
+
+class AccountsFragment : Fragment(R.layout.fragment_accounts) {
+
+}
---
+++ app/src/main/java/com/example/moneytreelighttest/accounts/AccountsRecyclerView.kt
@@ -0,0 +1,4 @@
+package com.example.moneytreelighttest.accounts
+
+class AccountsRecyclerView {
+}
