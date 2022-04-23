c3e6f74e1df47ff34c71ade9cb72daa5614169ae
Mikhail <beardykun@yahoo.com>
2022-04-23 11:57:34 +0900

    added Instrumented test to check navigation to TransactionsFragment

--- app/src/androidTest/java/com/example/moneytreelighttest/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/moneytreelighttest/ExampleInstrumentedTest.kt
@@ -1,12 +1,21 @@
 package com.example.moneytreelighttest
 
-import androidx.test.platform.app.InstrumentationRegistry
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.BoundedMatcher
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
+import com.example.moneytreelighttest.accounts.AccountsAdapter
+import org.hamcrest.Matcher
+import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +24,38 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.moneytreelighttest", appContext.packageName)
+    }
+
+    @Test
+    fun openTransactionsFragment() {
+        if (!recyclerViewSizeMatcher(0).matches(0))
+            onView(withId(R.id.rvAccounts)).perform(
+                RecyclerViewActions.actionOnItemAtPosition<AccountsAdapter.AccountsViewHolder>(
+                    0,
+                    click()
+                )
+            )
+    }
+
+    private fun recyclerViewSizeMatcher(matcherSize: Int): Matcher<View?> {
+        return object : BoundedMatcher<View?, RecyclerView>(RecyclerView::class.java) {
+
+            override fun matchesSafely(recyclerView: RecyclerView): Boolean {
+                return matcherSize == recyclerView.adapter?.itemCount
+            }
+
+            override fun describeTo(description: org.hamcrest.Description?) {
+                description?.appendText("with list size: $matcherSize")
+            }
+        }
     }
 }
