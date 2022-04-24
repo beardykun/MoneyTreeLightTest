1be2c97d7945717d40896284416997b34943db51
Mikhail <beardykun@yahoo.com>
2022-04-24 12:50:06 +0900

    added dependency for compose icons and updated Transactions object

--- app/src/main/java/com/example/moneytreelighttest/model/Transaction.kt
+++ app/src/main/java/com/example/moneytreelighttest/model/Transaction.kt
@@ -2,11 +2,21 @@ package com.example.moneytreelighttest.model
 
 import java.util.*
 
+
 data class Transaction(
-    private val accountId: Int,
-    private val amount: Double,
-    private val categoryId: Int,
-    private val date: Date,
-    private val description: String,
-    private val id: Int
-)
+    val accountId: Int,
+    val amount: Double,
+    val categoryId: Int,
+    val date: Date,
+    val description: String,
+    val id: Int
+) {
+    companion object {
+        const val ACCOUNT_ID = "account_id"
+        const val AMOUNT = "amount"
+        const val CATEGORY_ID = "category_id"
+        const val DATE = "date"
+        const val DESCRIPTION = "description"
+        const val ID = "id"
+    }
+}
